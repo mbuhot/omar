bf09886f32ada702647b3c187efb64cb52fa7377
Mike Buhot <m.buhot@gmail.com>
2019-02-28 20:19:49 +1000

    Initial umbrella skeleton

---
+++ README.md
@@ -0,0 +1,11 @@
+# Omar
+
+An umbrella application built to grow organically.
+
+## Goals
+
+ - Encourage pragmatic code organisation
+ - Code that will change together should be nearby
+ - It should be easy to test individual features and subsystems
+ - Avoid unnecessary resource usage (DB Connection pools, http server processes)
+ - Enable distillery releases for different combinations of apps
---
+++ config/config.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# By default, the umbrella project as well as each child
+# application will require this configuration file, as
+# configuration and dependencies are shared in an umbrella
+# project. While one could configure all applications here,
+# we prefer to keep the configuration of each individual
+# child application in their own app, but all other
+# dependencies, regardless if they belong to one or multiple
+# apps, should be configured in the umbrella to avoid confusion.
+import_config "../apps/*/config/config.exs"
+
